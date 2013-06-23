c37c738593de07e4bf29cb02f437c62dcaa5f8f0
psegarel <patrick@ps.dev>
2013-06-23 20:10:40 +0800

    Changes recipes' order in roles/base.rb

--- roles/base.rb
+++ roles/base.rb
@@ -2,11 +2,12 @@ name "base"
 description "Base role applied to all nodes."
 run_list(
   # "recipe[users::sysadmins]",
+  "recipe[chef-client::bluepill]",
   "recipe[sudo]",
   "recipe[yum]",
-  "recipe[git]",
   "recipe[build-essential]",
-  "recipe[vim]"
+  "recipe[vim]",
+  "recipe[git]",
 ) 
 override_attributes(
   :authorization => {
